16e9d37a185da6156d4d54e3834a85bd9724d13b
Nicolas Umiastowski <numiastowski@gmail.com>
2016-08-03 22:05:28 +0200

    Display list of questions

--- pre-project-form-submit-refactore.elm
+++ pre-project-form-submit-refactore.elm
@@ -20,16 +20,15 @@ main =
 
 type alias Model =
   { myScoreList : Dict Int Int
-    ,answerScore : List {myText: String, myValue: Int}
+    ,listOfAnswers : List {myText: String, myValue: Int}
     ,myTotalScore : Int
-    ,myQuestion1 : String
-    ,myQuestion2 : String
+    ,listOfQuestions :  List String
   }
  
 
 init :  (Model, Cmd Msg)
 init =
-  (Model Dict.empty [{myText = "Fully agree", myValue = 3}, {myText = "Agree", myValue = 2}, {myText = "Neutral", myValue = 1}, {myText = "Disagree", myValue=0}] 0 "Is the business co-located with the developers?" "Is the scope flexible?", Cmd.none)
+  (Model Dict.empty [{myText = "Fully agree", myValue = 3}, {myText = "Agree", myValue = 2}, {myText = "Neutral", myValue = 1}, {myText = "Disagree", myValue=0}] 0 ["Is the business co-located with the developers?", "Is the scope flexible?"], Cmd.none)
 
 calculateTotalScore : Dict comparable number -> number
 calculateTotalScore scorelist = Dict.foldl (\_ v sum -> v + sum) 0 scorelist
@@ -51,14 +50,18 @@ type Msg =
 
 view
     : { b
-          | answerScore : List { a | myText : String, myValue : Int }
+          | listOfAnswers : List { a | myText : String, myValue : Int }
+          , listOfQuestions : List String
           , myScoreList : Dict comparable number
     }
     -> Html Msg
 view model =
   let
+    displayListOfQuestions = 
+      List.map myQuestionsDiv model.listOfQuestions
+
     answerButtons =
-      List.map createAnswerButtons model.answerScore
+      List.map createAnswerButtons model.listOfAnswers
       
     scoreMessage =
       let total = List.sum <| Dict.values <| model.myScoreList in
@@ -68,28 +71,30 @@ view model =
         else ""
   in
     div []
-    <|answerButtons ++
+    <|displayListOfQuestions ++
+      answerButtons ++
       [ br [] []
       , text scoreMessage
-      ]
+      ] 
 
-
-
-
+myQuestionsDiv : String -> Html a
+myQuestionsDiv listOfQuestions = 
+  div []
+  [ text <| listOfQuestions]
 
 createAnswerButtons : { a | myText : String, myValue : Int } -> Html Msg
-createAnswerButtons valueOfTheScore = 
+createAnswerButtons answersPairs = 
     div[]
-    [ text (valueOfTheScore.myText)
-    , input [ type' "radio", name ("myChoice3"), onCheck (\_ -> QuestionAnswered 2 valueOfTheScore.myValue)] []
+    [ text (answersPairs.myText)
+    , input [ type' "radio", name ("myChoice3"), onCheck (\_ -> QuestionAnswered 2 answersPairs.myValue)] []
     ]
 --UPDATE
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
     case msg of 
-        QuestionAnswered  questionId valueOfTheScore ->
+        QuestionAnswered  questionId answersPairs ->
           let 
-            newScoreList = Dict.insert questionId valueOfTheScore model.myScoreList
+            newScoreList = Dict.insert questionId answersPairs model.myScoreList
         
           in 
           ({ model |  myScoreList = newScoreList }, Cmd.none )
