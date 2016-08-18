137fbcbaa9a9616c99ab8c006399539fbc565dd0
Nicolas Umiastowski <numiastowski@gmail.com>
2016-08-18 23:16:18 +0200

    extract the score to a function, so as to be able to make a unit test

--- Preprojectform.elm
+++ Preprojectform.elm
@@ -1,4 +1,4 @@
-module Preprojectform exposing (Model,init,view,update,Msg)
+module Preprojectform exposing (Model,init,view,update,Msg(..),myVariable)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -6,6 +6,7 @@ import Html.App as Html
 import Html.Events exposing (onClick, onCheck)
 import Dict exposing (..)
 import Debug
+-- import Debug
 
 
 
@@ -27,50 +28,61 @@ type alias Model =
   { myScoreList : Dict Int Int
     ,listOfAnswers : List { myText: String, myValue: Int}
     ,listOfQuestions : List { questionId: Int, questionText : String }
+    ,scoreMessage : String
+    ,errrorMessage : String
   }
  
 
 init :  (Model, Cmd Msg)
 init =
-  (Model Dict.empty [{myText = "Fuly agree", myValue = 3}, {myText = "Agree", myValue = 2}, {myText = "Neutral", myValue = 1}, {myText = "Disagree", myValue=0}]  [{questionId = 1, questionText = "Is the business co-located with the developers?"}, {questionId = 2, questionText = "Is the scope flexible?"}], Cmd.none)
+  (Model Dict.empty [{myText = "Fully agree", myValue = 3}, {myText = "Agree", myValue = 2}, 
+    {myText = "Neutral", myValue = 1}, {myText = "Disagree", myValue=0}]  [{questionId = 1, 
+    questionText = "Is the business co-located with the developers?"}, 
+    {questionId = 2, questionText = "Is the scope flexible?"}] "" "", Cmd.none)
 
 --MESSAGES
 type Msg = 
     QuestionAnswered Int Int
+    | Send Int
 
 --VIEW
 view
     : Model
     -> Html Msg
 
-view model =
-  let
-   --List.concatMap (\x -> text x :: List.map createAnswerButtons model.listOfAnswers) model.listOfQuestions
 
+view model =
+
+
+  let
     displayListOfQuestions = List.concatMap  (\question ->  text question.questionText :: List.map (createAnswerButtons question.questionId) model.listOfAnswers) model.listOfQuestions
     
     maxPointsPossible : Int
     maxPointsPossible = List.length model.listOfQuestions * 3
 
     myCurrentNumberofPoints = List.sum <| Dict.values <| model.myScoreList
-    _ = Debug.log "My current number of Points" myCurrentNumberofPoints
-    myTotalScore : Int
-    myTotalScore = (myCurrentNumberofPoints * 100) // maxPointsPossible
-  
+    
+    myTotalScore3 =   myTotalScore2 myCurrentNumberofPoints maxPointsPossible
+   
 
-    scoreMessage =
-      if myTotalScore  >= 75 then "Bravo"
-      else 
-        if myTotalScore  < 75 then "not so bad"
-        else ""
+  
   in
     div []
     <|displayListOfQuestions ++
       [ br [] []
-      , text ("Your score is: " ++ toString myTotalScore ++ "%")
+      , button [onClick (Send <| myTotalScore3 ) ] [text "Send"]
       , br [] []
-      , text scoreMessage
+      , br [] []
+      , text model.scoreMessage
+      , text model.errrorMessage
       ] 
+
+myTotalScore2 : Int -> Int -> Int
+myTotalScore2 points maxPoints = 
+    (points*100) // maxPoints
+
+myVariable : number
+myVariable = 10
 
 createAnswerButtons : Int -> { a | myText : String, myValue : Int } -> Html Msg
 createAnswerButtons questionId answersPair = 
@@ -79,11 +91,11 @@ createAnswerButtons questionId answersPair =
     , input [ type' "radio", name ("myChoice" ++ toString questionId), onCheck (\_ -> QuestionAnswered questionId answersPair.myValue)] []
     ]
 --UPDATE
-
 update
     : Msg
-    -> { a | myScoreList : Dict Int Int }
-    -> ( { a | myScoreList : Dict Int Int }, Cmd b )
+    -> Model
+    -> ( Model, Cmd b)
+
 update msg model = 
     case msg of 
         QuestionAnswered  questionId answersId ->
@@ -91,7 +103,26 @@ update msg model =
             newScoreList = Dict.insert questionId answersId model.myScoreList
      
           in 
-          ({ model |  myScoreList = newScoreList }, Cmd.none )
+          ({ model |  myScoreList = Debug.log "myscorelist" newScoreList }, Cmd.none )
+        
+        Send finalScore ->
+            let 
+            myAnsweredQuestions = List.length <| Dict.values <| model.myScoreList
+            newScoreMessage =
+                if  (myAnsweredQuestions == 2 && finalScore  >= 75) then 
+                    "Your score is " ++ toString (finalScore) ++ "% " ++ "Bravo"
+                else 
+                    if (myAnsweredQuestions == 2 && finalScore  < 75) then
+                     "Your score is " ++ toString finalScore ++ "% " ++  "Not so bad"
+                else ""
+            newErrorMessage = 
+                if ((List.length <| Dict.values <| model.myScoreList) < 2) then
+                    "You must answer all questions!"
+                else
+                    ""
+            in 
+            ({ model | scoreMessage = newScoreMessage , errrorMessage = newErrorMessage }, Cmd.none )
+
 
 --SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
